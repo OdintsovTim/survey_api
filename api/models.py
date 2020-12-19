bb306ceb57275cc1d82b5993ef228525e6ffd044
Odintsov Timur <timodin@yandex.ru>
2020-12-19 22:47:44 +0300

    api.models: add framework of Survey and Question models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    finish_date = models.DateField()
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    text = description = models.CharField(max_length=300)
+    response_type = models.CharField()
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='questions')
+
+    def __str__(self):
+        return self.text
