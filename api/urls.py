465d79ae7beebc9dd15b62e0c9d00e345873aa3a
Odintsov Timur <timodin@yandex.ru>
2020-12-19 23:25:56 +0300

    api.urls: add and register router

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+router = DefaultRouter()
+router.register(r'surveys', views.SurveyViewSet)
+router.register(r'questions', views.QuestionViewSet)
 
 urlpatterns = [
-
+    path('v1/', include(router.urls))
 ]
-
