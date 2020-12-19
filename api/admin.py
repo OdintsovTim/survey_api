e5869bbe83e577e178e55ecf5cd25731622cbe95
Odintsov Timur <timodin@yandex.ru>
2020-12-19 23:18:56 +0300

    api.admin: add SurveyAdmin and QuestionAdmin classes

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, Question
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'start_date', 'finish_date', 'description')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'response_type', 'survey')
+
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question, QuestionAdmin)
