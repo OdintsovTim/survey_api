bd865922b2784793b465955fff13cc133687392c
Odintsov Timur <timodin@yandex.ru>
2020-12-20 10:40:43 +0300

    api.serialixers: add AnswerSerializer class

--- api/serializers.py
+++ api/serializers.py
@@ -24,3 +24,23 @@ class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('text', 'question_type', 'survey', 'options')
         model = Question
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField('username', read_only=True)
+
+    def validate(self, attrs):
+        if attrs['question'].question_type == 'T' and not attrs['text_answer']:
+            raise ValidationError('You have to write answer')
+        if attrs['question'].question_type == 'SO' and not attrs['option_selection']:
+            raise ValidationError('You have to select one option')
+        if attrs['question'].question_type == 'SO' and not len(attrs['option_selection']):
+            raise ValidationError('You have to select one option')
+        if attrs['question'].question_type == 'MO' and not len(attrs['option_selection']):
+            raise ValidationError('You have to select one option')
+
+        return attrs
+
+    class Meta:
+        fields = ('user', 'question', 'text_answer', 'option_selection')
+        model = Answer
