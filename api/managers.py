3722a3be955de9809aca9d36310eeebe81bf5be3
Odintsov Timur <timodin@yandex.ru>
2020-12-22 02:37:55 +0300

    api.managers: add DoneSurveManager class

--- api/managers.py
+++ api/managers.py
@@ -7,3 +7,9 @@ class SurveyManager(models.Manager):
     def get_active_surveys(self):
         current_date = datetime.date.today()
         return self.filter(finish_date__gte=current_date)
+
+
+class DoneSurveyManager(models.Manager):
+    def get_own_done_surveys(self, user):
+        return self.filter(user__id=user.id)
+
